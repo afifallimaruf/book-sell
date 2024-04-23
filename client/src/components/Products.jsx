13eaefed807cbf5587bcb09e6e72afe5ad0a1d5e
afifallimaruf <afifalli18maruf@gmail.com>
2024-04-23 07:39:02 +0700

    membuat fitur show more & show less products

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -7,6 +7,8 @@ import { HiOutlineExclamationCircle } from "react-icons/hi";
 function Products() {
   const [books, setBooks] = useState([]);
   const [showModal, setShowModal] = useState(false);
+  const [showMore, setShowMore] = useState(true);
+  const [showLess, setShowLess] = useState(false);
   const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
@@ -32,11 +34,41 @@ function Products() {
       console.log(error.message);
     }
   }, []);
+
+  const handleShowMore = async () => {
+    const startIndex = books.length;
+
+    try {
+      const res = await fetch(
+        `http://localhost:8080/api/book/get-books?startIndex=${startIndex}`
+      );
+
+      const data = await res.json();
+
+      if (res.ok) {
+        setBooks((prev) => [...prev, data[0]]);
+        setShowLess(true);
+        setShowMore(false);
+        if (data.length < 5) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleShowLess = () => {
+    setBooks(() => books.slice(0, books.length - 1));
+    setShowLess(false);
+    setShowMore(true);
+  };
+
   return (
-    <div className="table-auto overflow-x-scroll mt-4 ml-8">
+    <div className="table-auto overflow-x-scroll mt-4 ml-8 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 ">
       {currentUser.isAdmin && books.length > 0 ? (
         <>
-          <Table hoverable className="shadow-md" stripped>
+          <Table hoverable className="shadow-md" stripped="true">
             <Table.Head>
               <Table.HeadCell>Date Created</Table.HeadCell>
               <Table.HeadCell>Image</Table.HeadCell>
@@ -49,49 +81,61 @@ function Products() {
                 <span>Edit</span>/<span>Delete</span>
               </Table.HeadCell>
             </Table.Head>
-            <Table.Body className="divide-y">
-              {books.map((book) => {
-                return (
-                  <>
-                    <Table.Row>
-                      <Table.Cell>
-                        {new Date(book.createdAt).toLocaleDateString()}
-                      </Table.Cell>
-                      <Table.Cell>
-                        <img
-                          src={book.image}
-                          alt=""
-                          className="w-30 h-40 object-cover"
-                        />
-                      </Table.Cell>
-                      <Table.Cell>{book.title}</Table.Cell>
-                      <Table.Cell>{book.author}</Table.Cell>
-                      <Table.Cell>{book.categories}</Table.Cell>
-                      <Table.Cell>{book.price}</Table.Cell>
-                      <Table.Cell>{book.stock}</Table.Cell>
-                      <Table.Cell>
-                        <div className="flex gap-3">
-                          <div className="bg-green-400 text-white p-1 rounded-md">
-                            <Link to={`/update/${book._id}`}>Edit</Link>
-                          </div>
-                          <div className="bg-red-400 text-white p-1 rounded-md">
-                            <span
-                              onClick={() => {
-                                setShowModal(true);
-                              }}
-                              className="cursor-pointer"
-                            >
-                              Delete
-                            </span>
-                          </div>
-                        </div>
-                      </Table.Cell>
-                    </Table.Row>
-                  </>
-                );
-              })}
-            </Table.Body>
+            {books.map((book) => (
+              <Table.Body className="divide-y" key={book._id}>
+                <Table.Row>
+                  <Table.Cell>
+                    {new Date(book.createdAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <img
+                      src={book.image}
+                      alt=""
+                      className="w-30 h-40 object-cover"
+                    />
+                  </Table.Cell>
+                  <Table.Cell>{book.title}</Table.Cell>
+                  <Table.Cell>{book.author}</Table.Cell>
+                  <Table.Cell>{book.categories}</Table.Cell>
+                  <Table.Cell>{book.price}</Table.Cell>
+                  <Table.Cell>{book.stock}</Table.Cell>
+                  <Table.Cell>
+                    <div className="flex gap-3">
+                      <div className="bg-green-400 text-white p-1 rounded-md">
+                        <Link to={`/update/${book._id}`}>Edit</Link>
+                      </div>
+                      <div className="bg-red-400 text-white p-1 rounded-md">
+                        <span
+                          onClick={() => {
+                            setShowModal(true);
+                          }}
+                          className="cursor-pointer"
+                        >
+                          Delete
+                        </span>
+                      </div>
+                    </div>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
           </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className="w-full text-teal-500 self-center text-sm py-7"
+            >
+              Show more
+            </button>
+          )}
+          {showLess && (
+            <button
+              onClick={handleShowLess}
+              className="w-full text-teal-500 self-center text-sm py-7"
+            >
+              Show less
+            </button>
+          )}
         </>
       ) : (
         <h1>No books yet!</h1>

187108eccc10bff34210b352b550de17a3732a67
afifallimaruf <afifalli18maruf@gmail.com>
2024-04-28 19:01:44 +0700

    membuat komponen History, NonFiction, SciFi, SelfDev, Fiction

---
+++ client/src/components/SelfDev.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from "../assets/banner.jpg"
+
+function SelfDev() {
+    return (
+        <>
+        <div>
+        <div className='relative'>
+            <img className='h-64 w-full object-cover rounded-md' src={Image} alt='' />
+            <div class="absolute inset-0 bg-gray-700 opacity-60 rounded-md"></div>
+            <div class="absolute inset-0 flex items-center justify-center">
+                <h2 class="text-white text-3xl font-bold">Self Development</h2>
+            </div>
+        </div>
+        </div>
+        </>
+    )
+}
+
+export default SelfDev

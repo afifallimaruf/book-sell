36b274ad2839d3413cc41570ed9b6efddad65e85
afifallimaruf <afifalli18maruf@gmail.com>
2024-04-29 09:27:52 +0700

    membuat komponen Fiction dan fungsionalitasnya

--- client/src/pages/Fiction.jsx
+++ client/src/pages/Fiction.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from "../assets/banner.jpg"
+import BookCard from '../components/BookCard'
+
 
 function Fiction() {
+    const [fictionCategory, setFictionCategory] = useState([])
+
+    useEffect(()=> {
+        const fetchData = async()=> {
+            try {
+                const res = await fetch("http://localhost:8080/api/book/get-books?category=fiction")
+
+                const data = await res.json()
+                
+                if (!res.ok) {
+                    console.log(data.message)
+                } else {
+                    setFictionCategory(data)
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData()
+    },[])
     return (
         <>
         <div>
@@ -11,6 +33,21 @@ function Fiction() {
             <div class="absolute inset-0 flex items-center justify-center">
                 <h2 class="text-white text-3xl font-bold">Fiction</h2>
             </div>
+        </div>
+        <div className='mt-8 items-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 md:gap-5 place-items-center'>
+        {
+            fictionCategory.length > 0 ? (
+                fictionCategory.map((book)=> (
+                    <div key={book._id}>
+                    <BookCard data={book} />
+                    </div>
+            ))
+            ) : (
+                <div className='place-items-center'>
+                    <h1 className='text-center'>there are no books yet!</h1>
+                </div>
+            )
+        }
         </div>
         </div>
         </>

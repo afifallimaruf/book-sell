f8a852b9dc5aefebc2b0dfe2f7d58e9d638e01af
afifallimaruf <afifalli18maruf@gmail.com>
2024-05-24 10:49:26 +0700

    menambahkan redux thunk

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import { thunk } from "redux-thunk";
 
 const rootReducer = combineReducers({
   user: userReducer,
@@ -17,8 +18,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }),
+  middleware: [thunk],
 });
 
 export const persistor = persistStore(store);

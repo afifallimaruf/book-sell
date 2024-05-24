9bda83bdfcad08153b8ad4ab21e3b8c7fd31b816
afifallimaruf <afifalli18maruf@gmail.com>
2024-05-24 10:40:20 +0700

    menambahkan fungsi addToCart dan removeCart di user.route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,13 @@
-const { logout } = require("../controllers/user.controller");
+const {
+  logout,
+  addToCart,
+  removeCart,
+} = require("../controllers/user.controller");
+const { verifyToken } = require("../utils/verifyToken");
 const router = require("express").Router();
 
 router.post("/logout", logout);
+router.post("/add-to-cart", verifyToken, addToCart);
+router.get("/remove-cart", verifyToken, removeCart);
 
-module.exports = logout;
+module.exports = router;

99a79576012ed3b86e5480122890d13de49e5a04
afifallimaruf <afifalli18maruf@gmail.com>
2024-05-27 14:27:10 +0700

    menambahkan route ke update user

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,6 +2,7 @@ const {
   logout,
   addToCart,
   removeCart,
+  updateUser,
 } = require("../controllers/user.controller");
 const { verifyToken } = require("../utils/verifyToken");
 const router = require("express").Router();
@@ -9,5 +10,6 @@ const router = require("express").Router();
 router.post("/logout", logout);
 router.post("/add-to-cart", verifyToken, addToCart);
 router.get("/remove-cart", verifyToken, removeCart);
+router.put("/update/:userId", verifyToken, updateUser);
 
 module.exports = router;

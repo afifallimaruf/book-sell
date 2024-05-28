1d54b5f7fa6a9514e577fa87c77d727e57887011
afifallimaruf <afifalli18maruf@gmail.com>
2024-05-28 11:07:32 +0700

    membuat model order

---
+++ api/models/order.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;

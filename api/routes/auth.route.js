d22f22c04231aa5492b86a7bb9a7078bfe6ae125
afifallimaruf <afifalli18maruf@gmail.com>
2024-05-28 09:33:33 +0700

    melengkapi auth route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,7 @@
-const router = require("express").Router()
-const { register, login } = require("../controllers/auth.controller")
+const router = require("express").Router();
+const { register, login } = require("../controllers/auth.controller");
 
-router.post("/register", register)
-router.post("/login", login)
+router.post("/register", register);
+router.post("/login", login);
 
-module.exports = router
+module.exports = router;
